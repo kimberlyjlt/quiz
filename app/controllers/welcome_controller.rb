c775b0c38b0576149bd6980f924c8ba1efe6de6d
Kimberly Larson <kimberlylarson@Kimberlys-MacBook-Pro.local>
2014-08-06 09:38:08 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,17 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def like
+  end
+
+  def bottles
+  end
+  
+end
---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :entry, :author, :created_at, :updated_at
